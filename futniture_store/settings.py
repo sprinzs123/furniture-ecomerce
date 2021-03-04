0b65e70a5ebd401c50cde2f33376bd80190735d7
sprinzs123 <sprinzs351@gmail.com>
2021-03-04 09:24:54 -0500

    fixed price filter with flexbox/static files served now

--- futniture_store/settings.py
+++ futniture_store/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'sdwedfadhrte432dfsdfdcvxhrt43qrfvvcbt54jyu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
